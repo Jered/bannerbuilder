6acec1502759801e4bcae93aaa811c3890cb0816
Jered Cuenco <jered.cuenco@instrument.com>
2018-05-11 16:02:36 -0700

    update GSAP CDN reference

--- gulpfile.js
+++ gulpfile.js
@@ -26,10 +26,9 @@ var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
 
 function getFolders(dir) {
-  return fs.readdirSync(dir)
-    .filter(function(file) {
-      return fs.statSync(path.join(dir, file)).isDirectory();
-    });
+  return fs.readdirSync(dir).filter(function(file) {
+    return fs.statSync(path.join(dir, file)).isDirectory();
+  });
 }
 
 function makesprite(variant, size, retina) {
@@ -39,37 +38,45 @@ function makesprite(variant, size, retina) {
   var spriteData;
 
   if (!retina) {
-    spriteData = gulp.src(origin + 'assets/sprites/*.*') // source path of the sprite images
-    .pipe(spritesmith({
-      imgName: 'txtsprite.png',
-      imgPath: 'assets/txtsprite.png',
-      cssName: 'txtsprite.css',
-      padding: 1
-    }))
-    .on('error', function(error){
-      console.log(cErr(error));
-    });
+    spriteData = gulp
+      .src(origin + 'assets/sprites/*.*') // source path of the sprite images
+      .pipe(
+        spritesmith({
+          imgName: 'txtsprite.png',
+          imgPath: 'assets/txtsprite.png',
+          cssName: 'txtsprite.css',
+          padding: 1
+        })
+      )
+      .on('error', function(error) {
+        console.log(cErr(error));
+      });
   } else {
-    spriteData = gulp.src(origin + 'assets/sprites/*.*') // source path of the sprite images
-    .pipe(spritesmith({
-      retinaSrcFilter: [origin + 'assets/sprites/*@2x.*'],
-      imgName: 'txtsprite.png',
-      retinaImgName: 'txtsprite2x.png',
-      imgPath: 'assets/txtsprite.png',
-      retinaImgPath: 'assets/txtsprite2x.png',
-      cssName: 'txtsprite.css',
-      padding: 1
-    }))
-    .on('error', function(error){
-      console.log(cErr(error));
-    });
+    spriteData = gulp
+      .src(origin + 'assets/sprites/*.*') // source path of the sprite images
+      .pipe(
+        spritesmith({
+          retinaSrcFilter: [origin + 'assets/sprites/*@2x.*'],
+          imgName: 'txtsprite.png',
+          retinaImgName: 'txtsprite2x.png',
+          imgPath: 'assets/txtsprite.png',
+          retinaImgPath: 'assets/txtsprite2x.png',
+          cssName: 'txtsprite.css',
+          padding: 1
+        })
+      )
+      .on('error', function(error) {
+        console.log(cErr(error));
+      });
   }
 
   spriteData.css.pipe(gulp.dest(dest)); // output path for the CSS
 
   spriteData.img
     .pipe(buffer())
-    .pipe(imagemin([pngquant({quality: '10-30', speed: 4})], {verbose: true}))
+    .pipe(
+      imagemin([pngquant({ quality: '10-30', speed: 4 })], { verbose: true })
+    )
     .pipe(gulp.dest(dest + 'assets/')); // output path for the sprite
 }
 
@@ -79,21 +86,22 @@ gulp.task('default', function() {
 });
 
 // Cleans the dist and dev folders
-gulp.task('clean', function () {
-  return del(['dist/**/*', 'dev/**/*'])
-    .then(function(paths) {
-      console.log(cTask('Cleaning...\n'), cInfo(paths.join('\n ')));
-    });
+gulp.task('clean', function() {
+  return del(['dist/**/*', 'dev/**/*']).then(function(paths) {
+    console.log(cTask('Cleaning...\n'), cInfo(paths.join('\n ')));
+  });
 });
 
 // lints the js files for errors
-gulp.task('lint', function () {
+gulp.task('lint', function() {
   console.log(cTask('Linting files...'));
   // ESLint ignores files with "node_modules" paths.
   // So, it's best to have gulp ignore the directory as well.
   // Also, Be sure to return the stream from the task;
   // Otherwise, the task may end before the stream has finished.
-  return gulp.src(['src/**/*.js'])
+  return (
+    gulp
+      .src(['src/**/*.js'])
       // eslint() attaches the lint output to the "eslint" property
       // of the file object so it can be used by other modules.
       .pipe(eslint())
@@ -102,7 +110,8 @@ gulp.task('lint', function () {
       .pipe(eslint.format())
       // To have the process exit with an error code (1) on
       // lint error, return the stream and pipe to failAfterError last.
-      .pipe(eslint.failAfterError());
+      .pipe(eslint.failAfterError())
+  );
 });
 
 /** take the src folder, iterate over the structure to two depths assuming: first level = variants, second level = sizes.
@@ -116,14 +125,15 @@ gulp.task('lint', function () {
 gulp.task('makesprites', function() {
   console.log(cTask('Making sprite sheets...'));
   console.log(cInfo('variant'), argv.v, cInfo(', size'), argv.s);
-  var retina = (argv.r === undefined) ? true : ((argv.r === 'false') ? false : true);
-  var variants = (argv.v === undefined) ? getFolders('src/variants/') : [argv.v];
+  var retina = argv.r === undefined ? true : argv.r === 'false' ? false : true;
+  var variants = argv.v === undefined ? getFolders('src/variants/') : [argv.v];
 
-  for (var i=0, vl=variants.length; i < vl; i++) {
+  for (var i = 0, vl = variants.length; i < vl; i++) {
     var variant = variants[i];
-    var sizes = (argv.s === undefined) ? getFolders('src/variants/' + variant): [argv.s];
+    var sizes =
+      argv.s === undefined ? getFolders('src/variants/' + variant) : [argv.s];
 
-    for (var j=0, sl=sizes.length; j < sl; j++) {
+    for (var j = 0, sl = sizes.length; j < sl; j++) {
       var size = sizes[j];
       makesprite(variant, size, retina);
     }
@@ -132,17 +142,17 @@ gulp.task('makesprites', function() {
 
 // take the src folder, iterate over the structure to two depths assuming: first level = variants, second level = sizes.
 // builds the src files into the dev folder. Concats JS and css
-gulp.task('build', ['clean','lint'], function () {
+gulp.task('build', ['clean', 'lint'], function() {
   console.log(cTask('Building Banners...'));
 
   var variants = getFolders('src/variants/');
   var fullmerge = merge();
 
-  for (var i=0, vl=variants.length; i < vl; i++) {
+  for (var i = 0, vl = variants.length; i < vl; i++) {
     var variant = variants[i];
     var sizes = getFolders('src/variants/' + variant);
 
-    for (var j=0, sl=sizes.length; j < sl; j++) {
+    for (var j = 0, sl = sizes.length; j < sl; j++) {
       var size = sizes[j];
       var folder = variant + '/' + size + '/';
       var origin = 'src/variants/' + folder;
@@ -150,35 +160,66 @@ gulp.task('build', ['clean','lint'], function () {
       var merged = merge();
 
       // move images
-      gulp.src(['src/global/assets/**', origin + 'assets/**', '!' + origin + 'assets/sprites/', '!' + origin + 'assets/sprites/**'])
+      gulp
+        .src([
+          'src/global/assets/**',
+          origin + 'assets/**',
+          '!' + origin + 'assets/sprites/',
+          '!' + origin + 'assets/sprites/**'
+        ])
         .pipe(gulp.dest(dest + 'assets/'));
 
       // move over any manifest.js files for FlashTalking ads to the root of each banner next to index.html
       gulp.src([origin + 'manifest.js']).pipe(gulp.dest(dest));
 
       // concat the styles
-      var styleStream = gulp.src(['src/global/styles/*.css', origin + '*.css'])
+      var styleStream = gulp
+        .src(['src/global/styles/*.css', origin + '*.css'])
         .pipe(concat('screen.css'))
         .pipe(gulp.dest(dest));
       merged.add(styleStream);
 
       // concat the javascript
-      var scriptStream = gulp.src(['src/global/scripts/**/*.js', origin + '*.js', '!' + origin + 'manifest.js'])
+      var scriptStream = gulp
+        .src([
+          'src/global/scripts/**/*.js',
+          origin + '*.js',
+          '!' + origin + 'manifest.js'
+        ])
         .pipe(concat('scripts.min.js'))
         .pipe(gulp.dest(dest));
       merged.add(scriptStream);
 
       // inject the style and JS as well as meta info
-      var injectStream = gulp.src(origin + '*.html')
-      .pipe ( inject(merged, {ignorePath: dest, addRootSlash: false}))
-      .pipe ( replace('{{author}}', pkg.author))
-      .pipe ( replace('{{description}}', pkg.description))
-      .pipe ( replace('{{version}}', pkg.version))
-      .pipe ( replace('{{title}}', pkg.meta.client + ' ' + pkg.meta.campaign + ' | ' + variant + ' | ' + size + ' | ' + pkg.version))
-      .pipe ( replace('{{width}}', size.substring(0,size.indexOf('x'))))
-      .pipe ( replace('{{height}}', size.substring(size.indexOf('x')+1, size.length)))
-      .pipe ( gulp.dest(dest))
-      .pipe(wait(100)); // insert a slight pause so the file system can write successfully before we zip if this task is part of the zip chain
+      var injectStream = gulp
+        .src(origin + '*.html')
+        .pipe(inject(merged, { ignorePath: dest, addRootSlash: false }))
+        .pipe(replace('{{author}}', pkg.author))
+        .pipe(replace('{{description}}', pkg.description))
+        .pipe(replace('{{version}}', pkg.version))
+        .pipe(
+          replace(
+            '{{title}}',
+            pkg.meta.client +
+              ' ' +
+              pkg.meta.campaign +
+              ' | ' +
+              variant +
+              ' | ' +
+              size +
+              ' | ' +
+              pkg.version
+          )
+        )
+        .pipe(replace('{{width}}', size.substring(0, size.indexOf('x'))))
+        .pipe(
+          replace(
+            '{{height}}',
+            size.substring(size.indexOf('x') + 1, size.length)
+          )
+        )
+        .pipe(gulp.dest(dest))
+        .pipe(wait(100)); // insert a slight pause so the file system can write successfully before we zip if this task is part of the zip chain
 
       fullmerge.add(injectStream);
     }
@@ -187,33 +228,42 @@ gulp.task('build', ['clean','lint'], function () {
   return fullmerge;
 });
 
-
-
 // take the src folder, iterate over the structure to two depths assuming: first level = variants, second level = sizes.
 // create zips per size for each variant and place in the dist folder
-gulp.task('zip', ['build'], function () {
+gulp.task('zip', ['build'], function() {
   console.log(cTask('Zipping Banners'));
   var zip = require('gulp-zip'); // zip files
   // var date = new Date().toISOString().replace(/[^0-9]/g, '');
   var merged = merge();
   var variants = getFolders('dev/');
 
-  for (var i=0, vl=variants.length; i < vl; i++) {
+  for (var i = 0, vl = variants.length; i < vl; i++) {
     var variant = variants[i];
     var sizes = getFolders('dev/' + variant);
 
-    for (var j=0, sl=sizes.length; j < sl; j++) {
+    for (var j = 0, sl = sizes.length; j < sl; j++) {
       var size = sizes[j];
       var folder = 'dev/' + variant + '/' + size + '/';
-      var filename = pkg.meta.client + '-' + pkg.meta.campaign + '-' + variant + '-' + size + '-v' + pkg.version + '.zip';
+      var filename =
+        pkg.meta.client +
+        '-' +
+        pkg.meta.campaign +
+        '-' +
+        variant +
+        '-' +
+        size +
+        '-v' +
+        pkg.version +
+        '.zip';
 
       //console.info(filename);
 
       // keep directory structure
-      var zipStream = gulp.src(folder + '**/*')
-      .pipe ( zip(filename))
-      .pipe ( filesize({title:filename, showFiles:true}))
-      .pipe ( gulp.dest('dist'));
+      var zipStream = gulp
+        .src(folder + '**/*')
+        .pipe(zip(filename))
+        .pipe(filesize({ title: filename, showFiles: true }))
+        .pipe(gulp.dest('dist'));
 
       merged.add(zipStream);
     }
@@ -227,7 +277,7 @@ gulp.task('zip', ['build'], function () {
   Does not pickup changes to sprite folders these must be updated manually to create new sprites
   @usage gulp serve
 **/
-gulp.task('serve', ['build'], function () {
+gulp.task('serve', ['build'], function() {
   console.log(cTask('Browser Sync ...'));
 
   browserSync.init({
@@ -239,15 +289,18 @@ gulp.task('serve', ['build'], function () {
   // images which don't require re-making sprites, rebuild everything and reload
   // the browser
   gulp.watch(
-    ['src/global/styles/*.css',
+    [
+      'src/global/styles/*.css',
       'src/global/scripts/**/*.js',
       'src/variants/**/*.html',
       'src/variants/**/*.js',
       'src/variants/**/*.css',
       'src/variants/**/assets/*.jpg',
       'src/variants/**/assets/*.png',
-      'src/variants/**/assets/*.gif'],
-        ['build', reload]);
+      'src/variants/**/assets/*.gif'
+    ],
+    ['build', reload]
+  );
 
   // DEPRECATED UNTIL A MORE ELEGANT SOLUTION. THIS TRIGGERS MULTIPLE BUILD AND RELOADS
   // // If sprites are updated, remake them and reload the page
